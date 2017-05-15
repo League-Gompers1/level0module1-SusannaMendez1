4aefb9057fa353d3718b2c9c4b1154a65d17b1ce
SusannaMendez1 <Susanna.Mendez@gompersstudent.org>
2017-05-15 14:56:46 -0700

    Add files via upload

---
+++ forloopgauntlet.java
@@ -0,0 +1,18 @@
+
+public class forloopgauntlet {
+
+public static void main(String[] args) {
+
+	for (int i = 0; i <= 10 ; i = i + 1) {
+	System.out.println(i + " ");
+	if (i% 2 ==0) {
+		System.out.println("even");
+	}else{
+		System.out.println("odd");
+		}
+	}
+}
+
+
+
+}
