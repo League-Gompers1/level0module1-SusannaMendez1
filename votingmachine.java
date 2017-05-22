f3a59cf3205290e1044d974adf3c6457b7eebf6a
SusannaMendez1 <Susanna.Mendez@gompersstudent.org>
2017-05-22 14:52:23 -0700

    Add files via upload

---
+++ votingmachine.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class votingmachine {
+public static void main(String[] args) {
+	for (int i = 100; i >= 1 ; i= 1 + 1) {
+		JOptionPane.showInputDialog(" Enter the name of the person you wil be voting for "
+				+ " Dwayne The Rock Johnson VS Bill Nye the Science Guy " );
+	
+	}	
+	
+	
+	
+	
+	
+	}	
+
+
+
+
+
+
+
+
+}
+
